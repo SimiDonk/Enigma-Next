84c7dd955c8742e85d6112b16307b9af8dfc0200
David Simon <sdavid0611@gmail.com>
2025-06-30 13:33:47 +0200

    case issue

---
+++ app/[locale]/ui/Navigation.tsx
@@ -0,0 +1,25 @@
+import LanguageSwitcher from "./LanguageSwitcher";
+import NavBar, { ToggleDrawer } from "./Navbar";
+export const Logo = () => {
+  return (
+    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+      <path
+        clipRule="evenodd"
+        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default function Navigation() {
+  return (
+    <div className="sticky top-0 z-50 flex flex-row items-center justify-between px-4 h-16 bg-gray-800">
+      <Logo />
+      <NavBar />
+      <ToggleDrawer />
+      <LanguageSwitcher className="hidden md:block bg-red-800 w-[4rem] h-[3rem] mr-6 rounded-md" />
+    </div>
+  );
+}
